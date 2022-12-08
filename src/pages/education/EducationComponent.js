658ca5796097026d6d2574560eb3aa18fcfb6ae8
Teoh Sin Yee <teohsinyee.cs@gmail.com>
2022-12-08 15:38:44 +0800

    Update EducationComponent.js

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -7,7 +7,7 @@ import Educations from "../../containers/education/Educations";
 import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
 import { competitiveSites } from "../../portfolio";
-import { certifications } from "../../portfolio";
+//import { certifications } from "../../portfolio";
 import "./EducationComponent.css";
 import { Fade } from "react-reveal";
 
