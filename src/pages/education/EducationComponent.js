b26b62c9d3563fcd208ef015f90d98b2e102d7fa
Teoh Sin Yee <teohsinyee.cs@gmail.com>
2022-12-08 15:35:21 +0800

    adjust cert

--- src/pages/education/EducationComponent.js
+++ src/pages/education/EducationComponent.js
@@ -3,7 +3,7 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Educations from "../../containers/education/Educations";
-import Certifications from "../../containers/certifications/Certifications";
+//import Certifications from "../../containers/certifications/Certifications";
 import CompetitiveSites from "../../components/competitiveSites/CompetitiveSites";
 import EducationImg from "./EducationImg";
 import { competitiveSites } from "../../portfolio";
@@ -34,14 +34,12 @@ class Education extends Component {
                 <h3 className="heading-sub-text" style={{ color: theme.text }}>
                   Basic Qualification and Certifcations
                 </h3>
-                <CompetitiveSites logos={competitiveSites.competitiveSites} />
+                  <CompetitiveSites logos={competitiveSites.competitiveSites} />
               </div>
             </div>
           </Fade>
           <Educations theme={this.props.theme} />
-          {certifications.certifications.length > 0 ? (
-            <Certifications theme={this.props.theme} />
-          ) : null}
+        
         </div>
         <Footer theme={this.props.theme} />
         <TopButton theme={this.props.theme} />
